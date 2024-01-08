fb62e0cb8830f73e8dd672b12d3f098a0d665cb7
sasaniii <sasaninjayathilake@gmail.com>
2024-01-08 16:36:19 +0530

    styles added

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -8,38 +8,22 @@ import CloseIcon from '@mui/icons-material/Close';
 import { ClickAwayListener } from '@mui/material';
 import { useState, useEffect } from 'react';
 import TaskList from './TaskList';
-import TaskManager from './TaskManager';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 600,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
-
 
 function MainPage() {
 
   const [open, setOpen] = useState(false);
-  //const handleOpen = () => setOpen(true);
-  // const handleClose = () => setOpen(false);
+
   const [taskAvailability, setTaskAvailability] = useState(false);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    // Retrieve tasks from localStorage on mount
+
     const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
     setTasks(storedTasks);
-   
+
   }, []);
 
-  console.log(tasks,'menna stored tasks');
+  console.log(tasks, 'menna stored tasks');
 
   const addTask = (newTask) => {
     // setTasks([...tasks, task]);
@@ -50,7 +34,7 @@ function MainPage() {
     // Store updated tasks in localStorage
     localStorage.setItem('tasks', JSON.stringify(updatedTasks));
   };
-  
+
   const deleteTask = (taskId) => {
     // Filter out the task with the specified ID
     const updatedTasks = tasks.filter((task) => task.id !== taskId);
@@ -73,56 +57,106 @@ function MainPage() {
   const handleClickAway = () => {
     setOpen(true);
   };
- 
+
 
   return (
-   <div>
-   <figure className="text-center">
-  <blockquote className="blockquote pt-4">
-    <h2>To Do Application</h2>
-  </blockquote>
-  <figcaption className="blockquote-footer">
-    Let us <cite title="Source Title">Bind Your Time</cite>
-  </figcaption>
-</figure>
-   {/* <h2 className="d-flex justify-content-center p-4"> Let's Get Started</h2> */}
-   <div  className='d-flex justify-content-center p-4' ><button onClick={handleOpen}  type="button" class="btn btn-primary">Add Your Task Here +</button></div>
-   {!taskAvailability ? (
-        <div className='d-flex justify-content-center p-4'>
-           <h2 className="d-flex justify-content-center p-4"> Let's Get Started </h2>
-        </div>
-      ) : (
+    <div style={styles.pageBg}>
+      <br/>
+      <div className='container' style={styles.container}>
+      <div className='d-flex justify-content-center'><img src="logot.png" alt="logo" width="200px" height="200px" /> </div>
+      <div className='d-flex justify-content-center'><img src="start.png" alt="logo" width="300px" height="40px" /> </div>
+      <div className='d-flex justify-content-center p-4' ><button onClick={handleOpen} type="button" style={styles.taskBtn}>ADD YOUR TASK HERE +</button></div>
+      </div>
         <div>
-         
-        <TaskList tasks={tasks} setTasks={setTasks} deleteTask={deleteTask} />
-          </div>
-      )}
+          <TaskList tasks={tasks} setTasks={setTasks} deleteTask={deleteTask} />
+        </div>
+      
 
-<Modal
-  open={open}
-  onClose={handleClose}
-  
-  // onClose={open}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-<ClickAwayListener onClickAway={handleOpen} >
+      <div>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <ClickAwayListener onClickAway={handleOpen} >
 
-  <Box sx={style} disableBackdropClick={true}>
-  <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          
-        >
-          <CloseIcon />
-        </IconButton>
-<TaskModal modalOpen={setOpen} addTask={addTask} setTaskAvailability={setTaskAvailability}/>
-  </Box>
-  </ClickAwayListener>
-</Modal>
+          <Box sx={styles.BoxStyle} disableBackdropClick={true}>
+            <div className="d-flex justify-content-end">
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              className="d-flex justify-content-end"
+            >
+              <CloseIcon />
+            </IconButton>
+            </div>
+            <TaskModal modalOpen={setOpen} addTask={addTask} setTaskAvailability={setTaskAvailability}/>
+                   </Box>
+        </ClickAwayListener>
+      </Modal>
+      </div>
 
-   </div>
+    
+    </div>
   );
 }
+
+const styles = {
+
+
+  boxStyle1 : {
+    backgroundImage: 'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)',
+    height:'800px',
+    width:'700px',
+  },
+
+  pageBg : {
+    height: '640px',
+    backgroundColor: '#3EECAC',
+    backgroundImage: 'linear-gradient(19deg, #3EECAC 0%, #EE74E1 100%)',
+    // overflow:'hidden',
+
+  },
+
+  container : {
+    width:'30%',
+    height:'55%',
+    border: '2px',
+    borderRadius: '8px',
+    padding: '5px',
+    backgroundColor: '#8BC6EC',
+    backgroundImage: 'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)',
+  },
+
+  taskBtn : {
+    backgroundColor: '#FA8BFF',
+    backgroundImage: 'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)',
+    border: '2px',
+    borderRadius: '8px',
+    padding: '5px',
+    height:'50px',
+    width:'200px',
+    color:'white',
+  },
+
+  modal : {
+    backgroundColor: '#FA8BFF',
+    backgroundImage: 'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)',
+  },
+  BoxStyle : {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  },
+
+  
+};
 
 export default MainPage;
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,46 +1,19 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-import { Search } from '@mui/icons-material';
 // import SearchBar from './TaskList/SearchBar';
 // import { Button } from 'bootstrap';
 import { Button } from '@mui/material';
 import EditModal from './EditModal';
 import CloseIcon from '@mui/icons-material/Close';
 import Modal from '@mui/material/Modal';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 600,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
+import dayjs from 'dayjs';
 
 
 function descendingComparator(a, b, orderBy) {
@@ -59,154 +32,29 @@ function getComparator(order, orderBy) {
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-// function stableSort(array, comparator) {
-//   const stabilizedThis = array.map((el, index) => [el, index]);
-//   stabilizedThis.sort((a, b) => {
-//     const order = comparator(a[0], b[0]);
-//     if (order !== 0) {
-//       return order;
-//     }
-//     return a[1] - b[1];
-//   });
-//   return stabilizedThis.map((el) => el[0]);
-// }
+// const headCells = [
 
-const headCells = [
-  
-  {
-    id: 'task',
-    numeric: false,
-    disablePadding: false,
-    label: 'Task',
-  },
-  {
-    id: 'dateTime',
-    numeric: true,
-    disablePadding: false,
-    label: 'Date and Time',
-  },
-  {
-    id: 'priority',
-    numeric: true,
-    disablePadding: false,
-    label: 'Priority',
-  }
-];
+//   {
+//     id: 'task',
+//     numeric: false,
+//     disablePadding: false,
+//     label: 'Task',
+//   },
+//   {
+//     id: 'dateTime',
+//     numeric: true,
+//     disablePadding: false,
+//     label: 'Date and Time',
+//   },
+//   {
+//     id: 'priority',
+//     numeric: true,
+//     disablePadding: false,
+//     label: 'Priority',
+//   }
+// ];
 
-// function EnhancedTableHead(props) {
-//   const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-//     props;
-//   const createSortHandler = (property) => (event) => {
-//     onRequestSort(event, property);
-//   };
 
-//   return (
-//     <TableHead>
-//       <TableRow>
-//         <TableCell padding="checkbox">
-//           <Checkbox
-//             color="primary"
-//             indeterminate={numSelected > 0 && numSelected < rowCount}
-//             checked={rowCount > 0 && numSelected === rowCount}
-//             onChange={onSelectAllClick}
-//             inputProps={{
-//               'aria-label': 'select all desserts',
-//             }}
-//           />
-//         </TableCell>
-//         {headCells.map((headCell) => (
-//           <TableCell
-//             key={headCell.id}
-//             align={headCell.numeric ? 'right' : 'left'}
-//             padding={headCell.disablePadding ? 'none' : 'normal'}
-//             sortDirection={orderBy === headCell.id ? order : false}
-//           >
-//             <TableSortLabel
-//               active={orderBy === headCell.id}
-//               direction={orderBy === headCell.id ? order : 'asc'}
-//               onClick={createSortHandler(headCell.id)}
-//             >
-//               {headCell.label}
-//               {orderBy === headCell.id ? (
-//                 <Box component="span" sx={visuallyHidden}>
-//                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-//                 </Box>
-//               ) : null}
-//             </TableSortLabel>
-//           </TableCell>
-//         ))}
-//       </TableRow>
-//     </TableHead>
-//   );
-// }
-
-// EnhancedTableHead.propTypes = {
-//   numSelected: PropTypes.number.isRequired,
-//   onRequestSort: PropTypes.func.isRequired,
-//   onSelectAllClick: PropTypes.func.isRequired,
-//   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-//   orderBy: PropTypes.string.isRequired,
-//   rowCount: PropTypes.number.isRequired,
-// };
-
-// function EnhancedTableToolbar(props) {
-//   const { numSelected } = props;
-
-//   return (
-//     <Toolbar
-//       sx={{
-//         pl: { sm: 2 },
-//         pr: { xs: 1, sm: 1 },
-//         ...(numSelected > 0 && {
-//           bgcolor: (theme) =>
-//             alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-//         }),
-//       }}
-//     >
-//       {numSelected > 0 ? (
-//         <Typography
-//           sx={{ flex: '1 1 100%' }}
-//           color="inherit"
-//           variant="subtitle1"
-//           component="div"
-//         >
-//           {numSelected} selected
-//         </Typography>
-//       ) : (
-//         <Typography
-//           sx={{ flex: '1 1 100%' }}
-//           variant="h6"
-//           id="tableTitle"
-//           component="div"
-//         >
-//           Task Summary
-//         </Typography>
-//       )}
-
-//       {numSelected > 0 ? (
-//         <Tooltip title="Delete">
-//           <IconButton>
-//             <DeleteIcon />
-//           </IconButton>
-//         </Tooltip>
-//       ) : (
-//         <Tooltip title="Filter list">
-//           <IconButton>
-//             <FilterListIcon />
-//           </IconButton>
-//         </Tooltip>
-//       )}
-//     </Toolbar>
-//   );
-// }
-
-// EnhancedTableToolbar.propTypes = {
-//   numSelected: PropTypes.number.isRequired,
-// };
 
 function SearchBar({ onSearchChange }) {
   return (
@@ -218,7 +66,7 @@ function SearchBar({ onSearchChange }) {
   );
 }
 
-export default function EnhancedTable( {tasks, setTasks, deleteTask} ) {
+export default function EnhancedTable({ tasks, setTasks, deleteTask }) {
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
   const [selected, setSelected] = React.useState([]);
@@ -227,83 +75,31 @@ export default function EnhancedTable( {tasks, setTasks, deleteTask} ) {
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [isEditModalOpen, setIsEditModalOpen] = React.useState(false);
   const [selectedTask, setSelectedTask] = React.useState(null);
-  const [searchQuery, setSearchQuery] = React.useState(''); // Step 1: Add state for search query
+  const [searchQuery, setSearchQuery] = React.useState('');
 
 
-  // const [tasks, setTasks] = useState([]);
-
-  // const handleEditModalOpen = (taskId) => {
-  //   setIsEditModalOpen(true);
-  // };
-  const handleSearchChange = (query) => {
-    setPage(0); // Reset page when search query changes
-    setSearchQuery(query); // Update search query state
-  };
-
-
-//   const handleDelete = (taskId) => {
-//     // Use filter to create a new array excluding the task with the specified ID
-// const updatedTasks = tasks.filter((task) => task.id !== taskId);
-
-// // Update the tasks array using setTasks
-// setTasks(updatedTasks);
-
-// // You may also want to update the local storage
-// localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-// const data = JSON.stringify(localStorage.getItem(taskId));
-
-// localStorage.removeItem(taskId);
-//   console.log('Delete clicked for task ID:', taskId);
-// };
-
-  
   const handleEdit = (taskId) => {
-    // taskId = tasks.id;
-    // Implement your logic for handling edit button click
-    
+
+
     setIsEditModalOpen(true);
     setSelectedTask(taskId);
     console.log('Edit clicked for task ID:', taskId);
 
   };
 
-      // Callback function to handle the updated task data
   const handleSaveEdit = (updatedTask) => {
-    // Find the task with the same ID in the tasks array
+
     const updatedTasks = tasks.map((task) =>
       task.id === updatedTask.id ? updatedTask : task
     );
 
-    // Update the tasks array using setTasks
     setTasks(updatedTasks);
 
     console.log('Updated Tasks:', updatedTasks);
-
-    // Close the modal
     handleClose();
   };
 
-  
 
-
-  // const handleRequestSort = (event, property) => {
-  //   const isAsc = orderBy === property && order === 'asc';
-  //   setOrder(isAsc ? 'desc' : 'asc');
-  //   setOrderBy(property);
-  // };
-
-  // const handleSelectAllClick = (event) => {
-  //   if (event.target.checked) {
-  //     const newSelected = rows.map((n) => n.id);
-  //     setSelected(newSelected);
-  //     return;
-  //   }
-  //   setSelected([]);
-  // };
-
-  // const addTask = (taskData) => {
-  //   setTasks((prevTasks) => [...prevTasks, taskData]);
-  // };
 
   const handleClick = (event, id) => {
     const selectedIndex = selected.indexOf(id);
@@ -324,176 +120,116 @@ export default function EnhancedTable( {tasks, setTasks, deleteTask} ) {
     setSelected(newSelected);
   };
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  // const handleChangeDense = (event) => {
-  //   setDense(event.target.checked);
-  // };
 
   const isSelected = (id) => selected.indexOf(id) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - tasks.length) : 0;
-
-  // const visibleRows = React.useMemo(
-  //   () =>
-  //     stableSort(tasks, getComparator(order, orderBy)).slice(
-  //       page * rowsPerPage,
-  //       page * rowsPerPage + rowsPerPage,
-  //     ),
-  //   [order, orderBy, page, rowsPerPage],
-  // );
 
   const handleClose = () => {
     setIsEditModalOpen(false);
   };
 
-
-  
-  
-
   return (
-   <div>
-    <br></br>
-     <div className='d-flex justify-content-end px-4 pb-2'><SearchBar onSearchChange={handleSearchChange}/></div>
+    <div>
+      <br></br>
+      {/* <div className='d-flex justify-content-end px-4 pb-2'><SearchBar onSearchChange={handleSearchChange} /></div> */}
 
-     <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        {/* <EnhancedTableToolbar numSelected={selected.length} /> */}
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 650 }}
-            aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}
-          >
-            {/* <EnhancedTableHead
-              // numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              // onSelectAllClick={handleSelectAllClick}
-              // onRequestSort={handleRequestSort}
-              rowCount={tasks.length}
-            /> */}
-            <TableBody>
-              {tasks.map((task, index) => {
-                const isItemSelected = isSelected(task.id);
-                const labelId = `enhanced-table-checkbox-${index}`;
+      <Box sx={{ width: '900px', paddingLeft:'20%' }}>
+        <Paper sx={{ width: '900px', mb: 2 }}>
+          {/* <EnhancedTableToolbar numSelected={selected.length} /> */}
+          <TableContainer>
+            <Table
+              sx={{ minWidth: 900 }}
+              aria-labelledby="tableTitle"
+              size={dense ? 'small' : 'medium'}
+            >
 
-                return (
-                  // {tasks.map((task, index) => (
+              <TableBody>
+                {tasks.map((task, index) => {
+                  const isItemSelected = isSelected(task.id);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    // {tasks.map((task, index) => (
                     <TableRow
-                    hover
-                    onClick={(event) => handleClick(event, task.id)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={index}
-                    selected={isItemSelected}
-                    sx={{ cursor: 'pointer' }}
-                  >
-                    {/* <TableCell padding="checkbox">
-                      <Checkbox
-                        color="primary"
-                        checked={isItemSelected}
-                        inputProps={{
-                          'aria-labelledby': labelId,
-                        }}
-                      />
-                    </TableCell>
+                      hover
+                      onClick={(event) => handleClick(event, task.id)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={index}
+                      selected={isItemSelected}
+                      sx={{ cursor: 'pointer' }}
+                    >
 
-                    // <TableCell
-                    //   component="th"
-                    //   id={labelId}
-                    //   scope="row"
-                    //   padding="none"
-                    // >
-                    //   {row.task}
-                    // 
-                    
-                    </TableCell> */}
-                    <TableCell align="right">{task.id}</TableCell>
-                    <TableCell align="right">{task.taskName}</TableCell>
-                    <TableCell align="right">{task.dateTime && task.dateTime.toString()}</TableCell>
-                    <TableCell align="right">{task.priority}</TableCell>
-                    {/* <TableCell align="right">Actions</TableCell> */}
-                    <TableCell align="right">
-          <Button variant="contained" color="primary" onClick={() => handleEdit(task)}>
-            Edit
-          </Button>
-        
-        </TableCell>
-        <TableCell align='right'>
-        <Button  color="secondary" onClick={() => deleteTask(task.id)}>
-            Delete
-          </Button>
-        </TableCell>
-                    
-                  </TableRow>
-                  // ))}
-                );
-              })}
-              {/* {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 33 : 53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )} */}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        {/* <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        /> */}
-      </Paper>
-      {/* <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      /> */}
-    </Box>
+                      {/* <TableCell align="right">{task.id}</TableCell> */}
+                      <TableCell align="right">{task.taskName}</TableCell>
+                      {/* <TableCell align="right">{task.dateTime.toString()}</TableCell> */}
+                      <TableCell align='right'>{dayjs(task.dateTime).format('YYYY-MM-DD HH:mm')}</TableCell>
+                      <TableCell align="right">{task.priority}</TableCell>
 
-    <Modal
-  open={isEditModalOpen}
-  onClose={handleClose}
-  
-  // onClose={open}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-{/* <ClickAwayListener onClickAway={handleOpen} > */}
+                      <TableCell align="right">
+                        <Button variant="contained" sx={{backgroundImage: 'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)', width:'100px'}} onClick={() => handleEdit(task)}>
+                          Edit
+                        </Button>
 
-  <Box sx={style} disableBackdropClick={true}>
-  <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          
-        >
-          <CloseIcon />
-        </IconButton>
-{/* <TaskModal modalOpen={setOpen} addTask={addTask} setTaskAvailability={setTaskAvailability}/> */}
-  <EditModal taskDetails={selectedTask} onSave={handleSaveEdit} editModalOpen={isEditModalOpen} onClose={handleClose} />
-  </Box>
-  {/* </ClickAwayListener> */}
-</Modal>
+                      </TableCell>
+                      <TableCell align='right'>
+                        <Button variant="contained" sx={{backgroundImage: 'linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)', width:'100px'}}  onClick={() => deleteTask(task.id)}>
+                          Delete
+                        </Button>
+                      </TableCell>
 
-   </div>
+                    </TableRow>
+                    // ))}
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+
+      </Box>
+
+      <Modal
+        open={isEditModalOpen}
+        onClose={handleClose}
+
+        // onClose={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+
+
+        <Box sx={styles.styling} disableBackdropClick={true}>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            className="d-flex justify-content-end"
+          >
+            <CloseIcon />
+          </IconButton>
+          {/* <TaskModal modalOpen={setOpen} addTask={addTask} setTaskAvailability={setTaskAvailability}/> */}
+          <EditModal taskDetails={selectedTask} onSave={handleSaveEdit} editModalOpen={isEditModalOpen} onClose={handleClose} />
+        </Box>
+        {/* </ClickAwayListener> */}
+      </Modal>
+
+    </div>
   );
+}
+
+const styles = {
+  styling : {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  },
+  
 }
